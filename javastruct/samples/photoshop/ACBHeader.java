79ab3d0562e8a1199a1ab7db6687799c639fea04
mdakin <mdakin@8804a3c7-b431-0410-8e5d-57a2fe0a61c1>
2007-07-18 07:02:09 +0000

    Adobe Color book reader-writer example.

---
+++ javastruct/samples/photoshop/ACBHeader.java
@@ -0,0 +1,92 @@
+package photoshop;
+
+import struct.StructClass;
+import struct.StructField;
+
+/**
+ * ACBHeader struct for Adobe Photoshop Color book (acb) files. 
+ * For complete format information please look at: http://magnetiq.com/docs/PhotoshopColorBook.txt
+ * @author mdakin
+ *
+ */
+@StructClass
+public class ACBHeader {
+	
+	public static final short RGB  = 0;
+	public static final short CMYK  = 2;	
+	public static final short LAB  = 7;
+	
+	// Signature is always ASCII characters "8BCB"
+	@StructField (order = 0)
+	public byte[] signature = {'8', 'B', 'C', 'B'};
+	
+	//Photoshop 7 only opens files with a version of 1 (0x0001) 
+	@StructField (order = 1)
+	public short fileVersion;
+	
+	// Unique identifier..
+	@StructField (order = 2)
+	public int colorBookIdentifier;
+	
+	@StructField (order = 3)
+	public AString title;
+	
+	@StructField (order = 4)
+	public AString colorNamePrefix;
+	
+	@StructField (order = 5)
+	public AString colorNamePostfix;
+
+	// Generally a copyright notice
+	@StructField (order = 6)
+	public AString description;
+	
+	@StructField (order = 7)
+	public short colorCount; 
+
+	// Photoshop displays colors page by page. This field specifies the
+	// maximum number of colors that can appear on a page.
+	@StructField (order = 8)
+	public short colorsPerPage; 
+
+	@StructField (order = 9)
+	public short pageSelectorOffset;
+	
+	/*
+	Color Space Identifier:
+    1 = HSB
+    3 = Pantone
+    4 = Focaltone
+    5 = Trumatch
+    6 = Toyo
+    8 = Grayscale
+    10 = HKS
+    */
+	@StructField (order = 10)
+	public short ColorSpaceID;
+	
+	public ACBHeader(String title, short colorSpace, int colorCount){
+		this.fileVersion = 0x0001;
+		this.title = new AString(title);
+		this.colorNamePrefix = new AString("la");
+		this.colorNamePostfix = new AString("lo");
+		this.description = new AString("copyright magnetiq ;)");
+		this.ColorSpaceID = colorSpace;
+		this.colorCount = (short) colorCount;
+		this.colorsPerPage = 16;
+		this.pageSelectorOffset = 0;
+	}
+	
+	public String toString(){
+		String str = "ACB Color Book";
+		str += "Title: " + this.title + "\n";
+		str += "Color Name Prefix: " + this.colorNamePrefix + "\n";
+		str += "Color Name Postfix: " + this.colorNamePostfix + "\n";
+		str += "Description: " + this.description + "\n";
+		str += "Color count: " + this.colorCount + "\n";
+		str += "Color per Page: " + this.colorsPerPage + "\n";
+		str += "Page Selector Offset: " + this.pageSelectorOffset + "\n";
+		str += "Color Space ID: " + this.ColorSpaceID + "\n";
+		return str;
+	}
+}
