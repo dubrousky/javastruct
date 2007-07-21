81f9309e90a17dc940a4ba557d1f770d71570278
mdakin <mdakin@8804a3c7-b431-0410-8e5d-57a2fe0a61c1>
2007-07-21 11:26:45 +0000

    Format code according to Eclipse default

--- trunk/javastruct/samples/photoshop/AdobeColorBook.java
+++ trunk/javastruct/samples/photoshop/AdobeColorBook.java
@@ -32,36 +32,37 @@ public class AdobeColorBook {
 	RGBColor[] rgbColors = null;
 	CMYKColor[] cmykColors = null;
 	LabColor[] labColors = null;
-	
-	public void read(String acbFile){
+
+	public void read(String acbFile) {
 		try {
 			FileInputStream fis = new FileInputStream(new File(acbFile));
 			header = new ACBHeader();
-			StructUnpacker up = JavaStruct.getUnpacker(fis, ByteOrder.BIG_ENDIAN);
+			StructUnpacker up = JavaStruct.getUnpacker(fis,
+					ByteOrder.BIG_ENDIAN);
 			up.readObject(header);
-			switch(header.ColorSpaceID){
-				case ACBHeader.RGB :
-					rgbColors = new RGBColor[header.colorCount];
-					for(int i=0; i<header.colorCount; i++){
-						rgbColors[i] = new RGBColor();
-						up.readObject(rgbColors[i]);
-					}
-					break;
-				case ACBHeader.CMYK :
-					cmykColors = new CMYKColor[header.colorCount];
-					for(int i=0; i<header.colorCount; i++){
-						cmykColors[i] = new CMYKColor();
-						up.readObject(cmykColors[i]);
-					}
-					break;
-				case ACBHeader.LAB :
-					labColors = new LabColor[header.colorCount];
-					for(int i=0; i<header.colorCount; i++){
-						labColors[i] = new LabColor();
-						up.readObject(labColors[i]);
-					}
-					break;
-				default:
+			switch (header.ColorSpaceID) {
+			case ACBHeader.RGB:
+				rgbColors = new RGBColor[header.colorCount];
+				for (int i = 0; i < header.colorCount; i++) {
+					rgbColors[i] = new RGBColor();
+					up.readObject(rgbColors[i]);
+				}
+				break;
+			case ACBHeader.CMYK:
+				cmykColors = new CMYKColor[header.colorCount];
+				for (int i = 0; i < header.colorCount; i++) {
+					cmykColors[i] = new CMYKColor();
+					up.readObject(cmykColors[i]);
+				}
+				break;
+			case ACBHeader.LAB:
+				labColors = new LabColor[header.colorCount];
+				for (int i = 0; i < header.colorCount; i++) {
+					labColors[i] = new LabColor();
+					up.readObject(labColors[i]);
+				}
+				break;
+			default:
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -69,69 +70,70 @@ public class AdobeColorBook {
 			e.printStackTrace();
 		}
 	}
-	
-	public void write(String name){
-		try{
+
+	public void write(String name) {
+		try {
 			FileOutputStream fo = new FileOutputStream(new File(name));
-			StructPacker packer = JavaStruct.getPacker(fo, ByteOrder.BIG_ENDIAN);
+			StructPacker packer = JavaStruct
+					.getPacker(fo, ByteOrder.BIG_ENDIAN);
 			packer.writeObject(header);
-			switch(header.ColorSpaceID){
-				case ACBHeader.RGB :
-					for(RGBColor color : rgbColors){
-						packer.writeObject(color);
-					}
-					break;
-				case ACBHeader.CMYK :
-					for(CMYKColor color : cmykColors){
-						packer.writeObject(color);
-					}
-					break;
-				case ACBHeader.LAB :
-					for(LabColor color : labColors){
-						packer.writeObject(color);
-					}
-					break;
-				default:
+			switch (header.ColorSpaceID) {
+			case ACBHeader.RGB:
+				for (RGBColor color : rgbColors) {
+					packer.writeObject(color);
+				}
+				break;
+			case ACBHeader.CMYK:
+				for (CMYKColor color : cmykColors) {
+					packer.writeObject(color);
+				}
+				break;
+			case ACBHeader.LAB:
+				for (LabColor color : labColors) {
+					packer.writeObject(color);
+				}
+				break;
+			default:
 			}
 			fo.close();
-		} catch(StructException e){
+		} catch (StructException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String str = "";
-		if (header != null){
+		if (header != null) {
 			str += header;
 		}
-		switch(header.ColorSpaceID){
-		case ACBHeader.RGB :
-			for(RGBColor color : rgbColors) {
+		switch (header.ColorSpaceID) {
+		case ACBHeader.RGB:
+			for (RGBColor color : rgbColors) {
 				str += color + "\n";
 			}
 			break;
-		case ACBHeader.CMYK :
-			for(CMYKColor color : cmykColors) {
+		case ACBHeader.CMYK:
+			for (CMYKColor color : cmykColors) {
 				str += color + "\n";
 			}
 			break;
-		case ACBHeader.LAB :
-			for(LabColor color : labColors) {
+		case ACBHeader.LAB:
+			for (LabColor color : labColors) {
 				str += color + "\n";
 			}
 			break;
 		}
 		return str;
 	}
-	
+
 	public static void main(String[] args) {
 		AdobeColorBook anpaBook = new AdobeColorBook();
 		anpaBook.read("ANPA Color.acb");
 		anpaBook.write("ANPA_my_Color.acb");
 		System.out.println(anpaBook);
-		
+
 		AdobeColorBook focoltoneBook = new AdobeColorBook();
 		focoltoneBook.read("FOCOLTONE.acb");
 		System.out.println(focoltoneBook);
