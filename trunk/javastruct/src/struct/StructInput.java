deb30d4f83a8b672a9415376b81d5bb4275cfc2f
mdakin <mdakin@8804a3c7-b431-0410-8e5d-57a2fe0a61c1>
2007-09-04 05:45:00 +0000

    unpacker refactorings

--- trunk/javastruct/src/struct/StructInput.java
+++ trunk/javastruct/src/struct/StructInput.java
@@ -1,30 +1,91 @@
 package struct;
 
-import java.io.DataInput;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.nio.ByteOrder;
 
-import struct.Constants.Primitive;
+public abstract class StructInput extends InputStream {
+  
+  public void readObject( Object obj) throws StructException{
+      if(obj == null)  throw new StructException("Struct objects cannot be null.");
+      StructData info = StructUtils.getStructInfo(obj);
+      Field[] fields = info.getFields();
 
-public abstract class StructInputStream extends InputStream {
-
-    DataInput dataInput;
-
-  protected void init( InputStream inStream, ByteOrder order) {
-      if ( order == ByteOrder.LITTLE_ENDIAN){
-          dataInput = new LEDataInputStream(inStream);
-      }
-      else {
-          dataInput = new DataInputStream( inStream );
+      for (Field currentField : fields) {
+			//System.out.println("Processing field: " + currentField.getName());
+			StructFieldData fieldData = info.getFieldData(currentField.getName());
+			if(fieldData == null) {
+				throw new StructException("Field Data not found for field: " + currentField.getName());
+			}        
+          int arrayLength = -1;
+          boolean lengthedArray = false;
+          try{
+          	if(info.isLenghtedArray(currentField)){
+          		Field f = info.getLenghtedArray(currentField.getName());
+          		StructFieldData lengthMarker = info.getFieldData(f.getName());
+          		if (lengthMarker.requiresGetterSetter()) {
+          			arrayLength = ((Number)lengthMarker.getGetter().invoke( obj, (Object[])null)).intValue();
+          		} else {
+          			arrayLength = ((Number)lengthMarker.getField().get(obj)).intValue();
+          		}
+	       			lengthedArray = true;
+          	}
+          	// For private and protected fields, use getFieldName or isFieldName
+          	if ( fieldData.requiresGetterSetter()){
+          		Method getter = fieldData.getGetter();
+          		Method setter = fieldData.getSetter();
+          		
+          		if(getter == null || setter == null){
+          			throw new StructException(" getter/setter required for : "+ currentField.getName());
+          		}
+          		
+          		if(lengthedArray && arrayLength >= 0){
+          			Object ret = Array.newInstance(currentField.getType().getComponentType(),arrayLength);
+          			setter.invoke(obj,new Object[]{ret});
+          			if(currentField.getType().getComponentType().isPrimitive() == false){
+          				Object[] array = (Object[])ret;
+          				for(int j=0; j<arrayLength; j++){
+          					array[j] =  currentField.getType().getComponentType().newInstance();
+          				}
+          			}
+          		}
+          		if(lengthedArray == false && currentField.getType().isArray()){
+          			if(getter.invoke (obj, (Object[])null) == null){
+          				throw new StructException("Arrays can not be null :"+ currentField.getName());
+          			}
+          		}
+          		readField(fieldData, getter, setter, obj);
+          	}
+          	// If public, use directly.
+          	else {
+          		if(lengthedArray && arrayLength >= 0) {
+          			Object ret = Array.newInstance(currentField.getType().getComponentType(),arrayLength);
+          			currentField.set(obj,ret);
+          			if(currentField.getType().getComponentType().isPrimitive() == false){
+          				Object[] array = (Object[])ret;
+          				for(int j=0; j<arrayLength; j++){
+          					array[j] =  currentField.getType().getComponentType().newInstance();
+          				}
+          			}
+          		}
+          		if(lengthedArray == false && currentField.getType().isArray()){
+          			if(currentField.get(obj)== null){
+          				throw new StructException("Arrays can not be null. : "+ currentField.getName());
+          			}
+          		}
+          		if(lengthedArray == false || (lengthedArray == true && arrayLength >= 0)){
+          			readField(fieldData, null, null, obj);
+          		}
+          	}
+          }
+          catch (Exception e) {
+               throw new StructException(e);
+          }
       }
   }
-
-  public abstract void readObject(Object obj) throws StructException;
 
   public void readField( StructFieldData fieldData, Method getter, Method setter, Object obj )
               throws IOException, InvocationTargetException, InstantiationException, IllegalAccessException, StructException {
@@ -167,81 +228,38 @@ public abstract class StructInputStream extends InputStream {
     return -1;
   }
 
-  protected boolean readBoolean() throws IOException {
-    return dataInput.readBoolean();
-  }
+  protected abstract boolean readBoolean() throws IOException;
 
-  protected byte readByte() throws IOException {
-    return dataInput.readByte();
-  }
+  protected abstract byte readByte() throws IOException;
 
-  protected short readShort() throws IOException {
-    return dataInput.readShort();
-  }
+  protected abstract short readShort() throws IOException;
 
-  protected int readInt() throws IOException {
-    return dataInput.readInt();
-  }
+  protected abstract int readInt() throws IOException;
 
-  protected long readLong() throws IOException {
-    return dataInput.readLong();
-  }
+  protected abstract long readLong() throws IOException;
 
-  protected char readChar() throws IOException {
-    return dataInput.readChar();
-  }
+  protected abstract char readChar() throws IOException;
 
-  protected float readFloat() throws IOException {
-    return dataInput.readFloat();
-  }
+  protected abstract float readFloat() throws IOException;
 
-  protected double readDouble() throws IOException {
-    return dataInput.readDouble();
-  }
+  protected abstract double readDouble() throws IOException;
 
-  protected void readBooleanArray(boolean buffer[]) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readBoolean();
-  }
+  protected abstract void readBooleanArray(boolean buffer[]) throws IOException;
 
-  protected void readByteArray( byte buffer[] ) throws IOException {
-      dataInput.readFully(buffer);
-  }
+  protected abstract void readByteArray( byte buffer[] ) throws IOException;
+  
+  protected abstract void readCharArray( char buffer[] ) throws IOException;
+  
+  protected abstract void readShortArray( short buffer[] ) throws IOException;
+  
+  protected abstract void readIntArray( int buffer[] ) throws IOException;
+  
+  protected abstract void readLongArray( long buffer[] ) throws IOException;
 
-  protected void readCharArray( char buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readChar();
-  }
+  protected abstract void readFloatArray( float buffer[] )throws IOException;
 
-  protected void readShortArray( short buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readShort();
-  }
+  protected abstract void readDoubleArray( double buffer[] )throws IOException;
 
-  protected void readIntArray( int buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readInt();
-  }
-
-  protected void readLongArray( long buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readLong();
-  }
-
-  protected void readFloatArray( float buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readFloat();
-  }
-
-  protected void readDoubleArray( double buffer[] ) throws IOException {
-    for ( int i=0; i<buffer.length; i++)
-      buffer[i] = readDouble();
-  }
-
-  protected void readObjectArray( Object objects[] )
-                 throws  StructException {
-    for ( int i=0; i<objects.length; i++)
-      readObject( objects[i] );
-  }
+  protected abstract void readObjectArray( Object objects[] )throws IOException, StructException;
 
 }
